3d8529f02afc20f94d5a867f8d860df8a4fb45af
Erik Örjehag <erik@orjehag.se>
2018-04-10 19:44:39 +0200

    node

---
+++ node/app.js
@@ -0,0 +1,45 @@
+var SerialPort = require("serialport");
+var MockBinding = SerialPort.Binding;
+
+var baudRate = 9600;
+var charDur = 11. / baudRate;
+var tSP = 2 * charDur;
+
+var port = new SerialPort("/dev/null", {
+  baudRate: baudRate
+});
+
+function sendTelegram(adress, data) {
+
+  var STX = 0x2; // Start byte
+
+  var LEN = data.length; // Data legth byte
+
+  var echo = 0; // Should slave respond with echo?
+  var broadcast = 0; // Broadcast to all slave?
+  var ADR = (echo << 6) | (broadcast << 5) | adress;
+
+  // The telegram message
+  var msg = [STX, LEN, ADR].concat(data);
+
+  // XOR check-sum
+  var BCC = msg.slice(1).reduce((acc, val) => acc ^ val, msg[0]);
+
+  msg.push(BCC);
+
+  var sendByte = function(msg) {
+    if (msg.length > 0) {
+      port.write(new Buffer([msg[0]]));
+      msg = msg.slice(1);
+      setTimeout(() => {
+        sendByte(msg);
+      }, charDur);
+    }
+  }
+
+  setTimeout(() => {
+    sendByte(msg);
+  }, tSP);
+}
+
+sendTelegram(0, [1, 2, 3]);
