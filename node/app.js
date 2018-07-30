218c677e22e4c58e33de1e35c5f6c688b3b93f50
Erik Örjehag <erik@orjehag.se>
2018-07-31 00:50:36 +0200

    homing

--- node/app.js
+++ node/app.js
@@ -1,99 +1,5 @@
-var SerialPort = require("serialport");
-var Readline = SerialPort.parsers.Readline
 
-var baudRate = 38400; // 38400 9600
-var deviceInverter = "/dev/ttyUSB0" // "/dev/cu.usbserial-FT1MJ3Q6";
-
-var portInverter = new SerialPort(deviceInverter, {
-  autoOpen: true,
-  baudRate: baudRate,
-  dataBits: 8,
-  stopBits: 1,
-  parity: 'even'
-});
-
-portInverter.on('error', function(err) {
-  console.log('Error inverter port: ', err.message);
-});
-
-// Read data that is available but keep the stream from entering "flowing mode"
-var readbuf = new Buffer([]);
-portInverter.on('readable', function () {
-  readbuf = Buffer.concat([readbuf, portInverter.read()]);
-  if (readbuf.length == 20) {
-    console.log('Response:', readbuf);
-    readbuf = new Buffer([]);
-  }
-});
-
-function sendTelegram(to, PPO) {
-
-  var STX = 0x2; // Start byte
-
-  var LEN = PPO.length + 2; // Lenght of (ADDR + PPO.. + BCC)
-
-  var echo = 0; // Should slave respond with echo?
-  var broadcast = (to == null) ? 1 : 0; // Broadcast to all slaves?
-  var address = (to == null) ? 0 : to;
-  var ADDR = (echo << 6) | (broadcast << 5) | address;
-
-  // The telegram message
-  var msg = [STX, LEN, ADDR].concat(PPO);
-
-  // XOR check-sum
-  var BCC = msg.slice(1).reduce((acc, val) => acc ^ val, msg[0]);
-
-  msg.push(BCC);
-
-  var buff = new Buffer(msg)
-
-  console.log("sendTelegram", to, buff);
-
-  portInverter.write(buff);
-}
-
-function word_from(bits) {
-  return bits.reduce((acc, position) => acc | (1 << position), 0);
-}
-
-function create_PKW(PKE, IND, PWE) {
-  return [
-    PKE >> 8 & 0xff,
-    PKE & 0xff,
-    IND >> 8 & 0xff,
-    IND & 0xff,
-    PWE >> 24 & 0xff,
-    PWE >> 16 & 0xff,
-    PWE >> 8 & 0xff,
-    PWE & 0xff,
-  ]
-}
-
-function create_PZD(STW, SW1, SW2, SW3) {
-  return [
-    STW >> 8 & 0xff,
-    STW & 0xff,
-    SW1 >> 8 & 0xff,
-    SW1 & 0xff,
-    SW2 >> 8 & 0xff,
-    SW2 & 0xff,
-    SW3 >> 8 & 0xff,
-    SW3 & 0xff
-  ]
-}
-
-function set_position(revolutions) {
-  var increments = Math.round(revolutions * 1000);
-  var STW = word_from([0, 1, 2, 3, 4, 5, 6, 10, 12]);
-  var PZD = create_PZD(STW, 0, increments >> 16, increments & 0xffff);
-  var PKW = create_PKW(0, 0, 0);
-  var PPO = create_PPO2(PKW, PZD);
-  sendTelegram(addr, PPO);
-}
-
-function create_PPO2(PKW, PZD) {
-  return PKW.concat(PZD);
-}
+var inverter = require("./inverter.js");
 
 var stdin = process.stdin;
 stdin.setRawMode(true);
@@ -101,7 +7,7 @@ stdin.resume();
 stdin.setEncoding( 'utf8' );
 
 var uiControl = false;
-var addr = null;
+var id = null;
 var pos = 0;
 var interval;
 var toggle = false;
@@ -116,14 +22,10 @@ stdin.on('data', function(key) {
 
   var data = null
 
-  if (key === 'q') // Go from "Switch-on disabled" -> "Ready for switch-on"
+  if (key === 'q')
   {
     uiControl = false;
-    var STW = word_from([1, 2, 10]);
-    var PZD = create_PZD(STW, 0, 0, 0);
-    var PKW = create_PKW(0, 0, 0);
-    var PPO = create_PPO2(PKW, PZD);
-    sendTelegram(null, PPO);
+    inverter.startup();
   }
   else if (key === 'p') // Start UI control
   {
@@ -131,11 +33,11 @@ stdin.on('data', function(key) {
   }
   else if (key === 'a')
   {
-    set_position(0);
+    inverter.set_position(id, 0);
   }
   else if (key === 'b')
   {
-    set_position(0.25);
+    inverter.set_position(id, 0.25);
   }
   else if (key === 'c')
   {
@@ -144,9 +46,9 @@ stdin.on('data', function(key) {
     } else {
       interval = setInterval(function () {
         if (toggle) {
-          set_position(0.3);
+          inverter.set_position(id, 0.3);
         } else {
-          set_position(-0.3);
+          inverter.set_position(id, -0.3);
         }
         toggle = !toggle;
       }, 5000);
@@ -154,23 +56,29 @@ stdin.on('data', function(key) {
   }
   else if (key === '0')
   {
-    addr = 0;
+    id = 0;
   }
   else if (key === '1')
   {
-    addr = 1;
+    id = 1;
   }
   else if (key === '2')
   {
-    addr = 2;
+    id = 2;
   }
   else if (key === '3')
   {
-    addr = 3;
+    id = 3;
   }
   else if (key === '4')
   {
-    addr = null;
+    id = null;
+  }
+  else if (key === 'r') {
+    inverter.start_reference_run(id);
+  }
+  else if (key === 't') {
+    inverter.finish_reference_run(id);
   }
 });
 
@@ -203,14 +111,8 @@ http.listen(8080, function () {
   console.log('listening on port 8080');
 });
 
-
 setInterval(function () {
   if (uiControl) {
-    var increments = Math.round((knobAngle / 360.0) * 1000);
-    var STW = word_from([0, 1, 2, 3, 4, 5, 6, 10, 12]);
-    var PZD = create_PZD(STW, 0, increments >> 16, increments & 0xffff);
-    var PKW = create_PKW(0, 0, 0);
-    var PPO = create_PPO2(PKW, PZD);
-    sendTelegram(addr, PPO);
+    inverter.set_position(id, knobAngle / 360.0);
   }
 }, 100);
---
+++ node/inverter.js
@@ -0,0 +1,152 @@
+
+var SerialPort = require("serialport");
+
+var baudRate = 38400; // 38400 9600
+var deviceInverter = "/dev/ttyUSB0" // "/dev/cu.usbserial-FT1MJ3Q6";
+
+var portInverter = new SerialPort(deviceInverter, {
+  autoOpen: true,
+  baudRate: baudRate,
+  dataBits: 8,
+  stopBits: 1,
+  parity: 'even'
+});
+
+portInverter.on('error', function(err) {
+  console.log('Error inverter port: ', err.message);
+});
+
+// Read data that is available but keep the stream from entering "flowing mode"
+var readbuf = new Buffer([]);
+portInverter.on('readable', function () {
+  readbuf = Buffer.concat([readbuf, portInverter.read()]);
+  if (readbuf.length == 20) {
+    console.log('Response:', readbuf);
+    readbuf = new Buffer([]);
+  }
+});
+
+function sendTelegram(id, PPO) {
+
+  var STX = 0x2; // Start byte
+  var LEN = PPO.length + 2; // Lenght of (ADDR + PPO.. + BCC)
+
+  var echo = 0; // Should slave respond with echo?
+  var broadcast = 0; // Broadcast to all slaves?
+  var address = id;
+  if (id == null) {
+    broadcast = 1;
+    address = 0;
+  }
+
+  var ADDR = (echo << 6) | (broadcast << 5) | address;
+
+  // The telegram message
+  var msg = [STX, LEN, ADDR].concat(PPO);
+
+  // XOR check-sum
+  var BCC = msg.slice(1).reduce((acc, val) => acc ^ val, msg[0]);
+
+  msg.push(BCC);
+
+  var buff = new Buffer(msg)
+  console.log("sendTelegram", id, buff);
+  portInverter.write(buff);
+}
+
+function word_from(bits) {
+  return bits.reduce((acc, position) => acc | (1 << position), 0);
+}
+
+function create_PKW(PKE, IND, PWE) {
+  return [
+    PKE >> 8 & 0xff,
+    PKE & 0xff,
+    IND >> 8 & 0xff,
+    IND & 0xff,
+    PWE >> 24 & 0xff,
+    PWE >> 16 & 0xff,
+    PWE >> 8 & 0xff,
+    PWE & 0xff,
+  ]
+}
+
+function create_PZD(STW, SW1, SW2, SW3) {
+  return [
+    STW >> 8 & 0xff,
+    STW & 0xff,
+    SW1 >> 8 & 0xff,
+    SW1 & 0xff,
+    SW2 >> 8 & 0xff,
+    SW2 & 0xff,
+    SW3 >> 8 & 0xff,
+    SW3 & 0xff
+  ]
+}
+
+function create_PPO2(PKW, PZD) {
+  return PKW.concat(PZD);
+}
+
+function speed(decimal) {
+  return 0x4000 * decimal;
+}
+
+exports.startup = function () {
+  // Go from "Switch-on disabled" -> "Ready for switch-on"
+  var STW = word_from([1, 2, 10]);
+  var PZD = create_PZD(STW, 0, 0, 0);
+  var PKW = create_PKW(0, 0, 0);
+  var PPO = create_PPO2(PKW, PZD);
+  sendTelegram(null, PPO);
+};
+
+exports.set_position = function (id, revolutions) {
+  var increments = Math.round(revolutions * 1000) * (id == 0 ? 1 : -1);
+  var STW = word_from([
+    0, 1, 2, 3, 4, 5, 6, 10 // enable
+  ]);
+  var PZD = create_PZD(STW, 0, increments >> 16, increments & 0xffff);
+  var PKW = create_PKW(0, 0, 0);
+  var PPO = create_PPO2(PKW, PZD);
+  sendTelegram(id, PPO);
+};
+
+exports.start_reference_run = function (id) {
+  var STW = word_from([
+    0, 1, 2, 3, 4, 5, 6, 10, // enable
+    id == 0 ? 12 : 11, // direction left/right
+    8 // reference run
+  ]);
+  var PZD = create_PZD(STW, speed(0.05), 0, 0);
+  var PKW = create_PKW(0, 0, 0);
+  var PPO = create_PPO2(PKW, PZD);
+  sendTelegram(id, PPO);
+};
+
+exports.finish_reference_run = function (id) {
+  var STW = word_from([
+    0, 1, 2, 3, 4, 5, 6, 10, // enable
+    id == 0 ? 12 : 11, // direction left/right
+    8, // reference run
+    9 // hit home position
+  ]);
+  var PZD = create_PZD(STW, speed(0.05), 0, 0);
+  var PKW = create_PKW(0, 0, 0);
+  var PPO = create_PPO2(PKW, PZD);
+  sendTelegram(id, PPO);
+
+  // This will toggle the 9:th bit off again and
+  // complete the reference run. Think of bit 9
+  // as a homing push button.
+  setTimeout(function () {
+    exports.start_reference_run(id);
+
+    // Need to do this to break out from reference
+    // run squence. Can not do multiple reference
+    // runs after each other otherwise.
+    setTimeout(function () {
+      exports.set_position(id, 0);
+    }, 100);
+  }, 100);
+};
