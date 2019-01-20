afc88faf6f9efd3ecd16115d1453ef8e7e16c79b
Erik Örjehag <erik@Eriks-MacBook-Air.local>
2019-01-20 19:11:36 +0100

    public

--- node/app.js
+++ node/app.js
@@ -11,7 +11,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 socket.interface(io);
 
