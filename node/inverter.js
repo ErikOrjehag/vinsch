4a755fc5f3206dddc265f2851db289626d6ff3f5
Erik Örjehag <erik@Eriks-MacBook-Air.local>
2019-01-20 17:51:04 +0100

    argv

--- node/inverter.js
+++ node/inverter.js
@@ -4,7 +4,10 @@ var utils = require("./utils.js");
 var db = require("./db.js");
 
 var baudRate = 38400;
-var deviceInverter = "/dev/tty.usbserial-FT1MJ3Q6"; //"/dev/ttyUSB0";
+var deviceInverter = "/dev/tty.usbserial-FT1MJ3Q6";
+if (process.argv.length > 2) {
+  deviceInverter = process.argv[2];
+}
 
 var WHEEL_RADIUS = [0.273, 0.25, 0.25, 0.25]
 
