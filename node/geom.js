28ce01a9cc8e91b6777ab28231eea5c42b4e9a7c
Erik Örjehag <erik@orjehag.se>
2019-08-10 14:57:51 +0200

    cleanup move

--- node/geom.js
+++ node/geom.js
@@ -4,34 +4,40 @@ var utils = require("./utils.js");
 var socket = require("./socket.js");
 var db = require("./db.js");
 
-var pp = {
-  "TRI": [
-    { x: 1.82, y: 0.1, z: 2.37 },
-    { x: 1.82, y: 0, z: 2.37 },
-    { x: -1.82, y: -2.06, z: 2.37 },
-    { x: -1.82, y: 2.06, z: 2.37 }
-  ],
-  "QUAD": [
-    { x: 1.82, y: 2.06, z: 2.37 },
-    { x: 1.82, y: -2.06, z: 2.37 },
-    { x: -1.82, y: -2.06, z: 2.37 },
-    { x: -1.97, y: 2.06, z: 2.37 }
-  ]
-};
+// # # # These parameters are fetched from the database # # #
 
+// The layout of the rope pulleys
+var pp = [
+  { x: 1.82, y: 2.06, z: 2.37 },
+  { x: 1.82, y: -2.06, z: 2.37 },
+  { x: -1.82, y: -2.06, z: 2.37 },
+  { x: -1.97, y: 2.06, z: 2.37 }
+];
+
+// The home position
 var home = { x: 0, y: 0, z: 0.5 };
 
-var mode = "QUAD";
-
+// Multiplier to compensate for slack in the ropes, a lower value will tighten
 var SLACK = 1.0;
+
+// Where we want to be
 var setpoint = { x: 0, y: 0, z: 0 };
 
+exports.get_setpoint = function () {
+  return setpoint;
+};
+
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+// Used to implement linear_to function, that cuts up motion into small steps.
 var linear_active = false;
 
+// Helper to get current time in seconds
 function time() {
   return Date.now() / 1000.0;
 }
 
+// Fetch from database once on startup
 setTimeout(function () {
   db.get_setpoint(function (err, point) {
     if (err) console.log(err);
@@ -49,54 +55,27 @@ setTimeout(function () {
   });
 }, 100);
 
-exports.get_setpoint = function () {
-  return setpoint;
-};
-
 exports.set_layout = function (layout) {
-  pp["QUAD"] = layout.inverters;
+  pp = layout.inverters;
   home = layout.home;
   SLACK = layout.slack;
   console.log("home", home);
-  console.log("inverters", pp["QUAD"]);
+  console.log("inverters", pp);
   console.log("slack", SLACK);
 };
 
-async function go_to_specific(id, point, speed, h) {
-  var p = pp[mode];
-
-  if (mode == "TRI") {
-    if (h == undefined) h = Math.min(p[1].z, p[2].z, p[3].z) - 1.0;
-  } else if (mode == "QUAD") {
-    h = point.z;
-  }
+async function go_to_specific(id, point, speed) {
 
   if (speed == undefined) speed = 1;
 
+  // Pythagoras
   var a = Math.sqrt(
-    Math.pow(point.x - p[id].x, 2) +
-    Math.pow(point.y - p[id].y, 2));
-  var b = p[id].z - h;
+    Math.pow(point.x - pp[id].x, 2) +
+    Math.pow(point.y - pp[id].y, 2));
+  var b = pp[id].z - point.z;
   var c = Math.sqrt(a*a + b*b) * SLACK;
 
-  var len = 0;
-
-  if (mode == "TRI") {
-    if (id == 0) {
-      var l = h - point.z;
-      len = c + l;
-    } else {
-      len = c;
-    }
-  } else if (mode == "QUAD") {
-      //console.log(id)
-      len = c;
-  }
-
-  //console.log("id", id, "a", a, "b", b, "c", c, "p[id].z", p[id].z, "h", h);
-
   await inverter.set_length(id, c, speed);
-
 };
 
 exports.home_specific = async function (id) {
@@ -110,21 +89,6 @@ exports.go_to = async function (point, speed) {
   setpoint.z = point.z;
 
   socket.send_position(setpoint);
-
-  /* No bound checking, request by Mattias.
-  var p = pp[mode];
-  var pad = 0.7;
-  x_pos_bound = Math.min(p[0].x, p[1].x) - pad;
-  x_neg_bound = Math.max(p[2].x, p[3].x) + pad;
-  y_pos_bound = Math.min(p[0].y, p[3].y) - pad;
-  y_neg_bound = Math.max(p[1].y, p[2].y) + pad;
-  z_pos_bound = 0.8 * Math.min(p[0].z, p[1].z, p[2].z, p[3].z);
-  if (setpoint.x > x_pos_bound) { console.warn("setpoint.x out of positive bounds!"); setpoint.x = x_pos_bound; }
-  if (setpoint.x < x_neg_bound) { console.warn("setpoint.x out of negative bounds!"); setpoint.x = x_neg_bound; }
-  if (setpoint.y > y_pos_bound) { console.warn("setpoint.y out of positive bounds!"); setpoint.y = y_pos_bound; }
-  if (setpoint.y < y_neg_bound) { console.warn("setpoint.y out of negative bounds!"); setpoint.y = y_neg_bound; }
-  //if (setpoint.z > z_pos_bound) { console.warn("setpoint.z out of positive bounds!"); setpoint.z = z_pos_bound; }
-  */
 
   for (var i = 0; i < 4; i++) {
     await go_to_specific(i, setpoint, speed);
